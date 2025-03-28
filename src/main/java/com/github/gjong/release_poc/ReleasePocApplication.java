ead9f6fb535234d8ccb45a88d522f9983293092f
Gerben Jongerius <gerben.jongerius@opencirclesolutions.nl>
2025-03-28 13:48:38 +0100

    Simple POC to attempt and detect versions.

---
+++ src/main/java/com/github/gjong/release_poc/ReleasePocApplication.java
@@ -0,0 +1,13 @@
+package com.github.gjong.release_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasePocApplication.class, args);
+	}
+
+}
